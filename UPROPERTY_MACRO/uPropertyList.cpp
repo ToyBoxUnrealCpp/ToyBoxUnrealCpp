9d1812112c33f5921db28992d3957e4b5da53e8d
ToyBoxUnrealCpp <you.o.toyboxunreal@gmail.com>
2017-09-25 19:37:54 +0900

    update: UPROPERTY_MACRO dir

--- UPROPERTY_MACRO/uPropertyList.cpp
+++ UPROPERTY_MACRO/uPropertyList.cpp
@@ -8,27 +8,16 @@ AuPropertyList::AuPropertyList()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AuPropertyList::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void AuPropertyList::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-}
-
-
-// Ref:	Only valid on function param declaration.
-//		Value is copied out after function call.
-void AuPropertyList::meaninglessFunction(UPROPERTY(Ref)int32 reference)
-{
-	reference = 114514;
 }
--- UPROPERTY_MACRO/uPropertyList.h
+++ UPROPERTY_MACRO/uPropertyList.h
@@ -5,6 +5,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "meaninglessObject.h"
 #include "uPropertyList.generated.h"
 
 // Multicast Delegate Type
@@ -28,8 +29,6 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	void noProcessDelegate() {}
 
 public:
 	// EditAnywhere:	You can modify this property from Level Editor's Details Panel and Blueprint Editor's Details Panel.
@@ -99,7 +98,7 @@ public:
 
 	// BlueprintReadWrite:	This property can be read or written from a Blueprint.
 	//						This operator is incompatible with the BlueprintReadOnly specifier.
-	UPROPERTY(BlueprintReadWrite, VisibleDefaultsOnly)
+	UPROPERTY(BlueprintReadWrite)
 	// BlueprintReadWriteで、ブループリントで読み込んだり変更したりできるようになります。
 	// BlueprintReadOnlyとは同時に使えません。
 	int32 m_blueprintReadWrite;
@@ -116,35 +115,77 @@ public:
 	// 可変長配列の長さをエディタから変更できないようにします。
 	TArray<int32> m_editFixedSize = { 1, 2, 3 };
 
-	// Ref:	Only valid on function param declaration.
-	//		Value is copied out after function call.
-	// Ref指定子は関数の引数にしか宣言できません。
-	// この指定子をつけた引数はその値を関数が呼ばれた後も保存します。
-	int32 m_ref = 0;
-	void meaninglessFunction(UPROPERTY(Ref)int32 reference);
-
-	// Export:	Only useful for object properties (or arrays of objects). 
-	//			Indicates that the object assigned to this property should be exported in its entirety as a subobject block when the object is copied (for copy/paste) or exported to T3D,
-	//			as opposed to just outputting the object reference itself.
-	UPROPERTY(Export)
-	UObject* m_export;
-
 	// BlueprintAssignable:	This specifier can use with Multicast Delegates only.
 	//						Exposes the property for assigning in Blueprints.
-	UPROPERTY(BlueprintAssignable, Category = "Delegate")
+	UPROPERTY(BlueprintAssignable, Category = "Delegate|Bind")
 	// この指定子はマルチキャストデリゲート型でしか使えません。
 	// BlueptrintAssignableで、この変数をブループリントにバインド可能なデリゲートとして公開することができます。
 	FmeaninglessDelegate m_blueprintAssignable;
 
 	// BlueprintCallable:	This specifier can use with Multicast Delegates only.
 	//						This property should be exposed for calling in Blueprint code.
-	UPROPERTY(BlueprintCallable, Category = "Delegate")
+	UPROPERTY(BlueprintCallable, Category = "Delegate|Call")
 	// この指定子はマルチキャストデリゲート型でしか使えません。
 	// BlueptrintCallableで、この変数をブループリントでコール可能なデリゲートとして公開することができます。
 	FmeaninglessDelegate m_blueprintCallable;
 
+	// This delegate accepts(only in blueprint) only events with BlueprintAuthorityOnly.
+	UPROPERTY(BlueprintAuthorityOnly, BlueprintReadWrite)
+	// BlueprintAuthorityOnlyのイベントのみ受け付けます。
+	FmeaninglessDelegate m_blueprintAuthorityOnly;
+
+	// Object properties only. 
+	// When an instance of this class is created, it will be given a unique copy of the object assigned to this variable in defaults.
+	UPROPERTY(Instanced, EditAnywhere)
+	// オブジェクト限定
+	// このクラスのインスタンスが生成されたとき、インスタンスにはこの変数に設定されているオブジェクトのコピーが生成される。
+	class UStaticMeshComponent* m_instanced;
+
+	// Property should be serialized for save game.
+	UPROPERTY(SaveGame)
+	// セーブするためにこの変数はシリアライズされなければなりません。
+	int32 m_SaveGame;
+
+	// Transient:	Property is transient: shouldn't be saved, zero-filled at load time. Opposite of the SaveGame specifier.
+	UPROPERTY(Transient, EditAnywhere, Category = "Transient|Transient")
+	// Transientでシリアライズ不可な変数にすることができます。
+	// SaveGame指定子と同時には扱えません。
+	int32 m_transient;
+
+	// DuplicateTransient:	Property should always be reset to the default value during any type of duplication (copy/paste, binary duplication, etc.)
+	UPROPERTY(DuplicateTransient, EditAnywhere, Category = "Transient|DuplicateTransient")
+	// DuplicateTransient指定子で、複製時(コピー/ペーストとか)のシリアライズをできなくします。
+	int32 m_duplicateTransient;
+
+	// Property should always be reset to the default value unless it's being duplicated for a PIE session.
+	UPROPERTY(NonPIEDuplicateTransient, EditAnywhere, Category = "Transient|NonPIEDuplicateTransient")
+	// PIE(Play in Editor)セッション用に複製していないとき、シリアライズを禁止します。
+	int32 m_nonPIETransient;
+
+	UPROPERTY(TextExportTransient, EditAnywhere, Category = "Transient|TextExportTransient")
+		int32 m_textExportTransient;
+
+	UPROPERTY(EditAnywhere, Category = "Clear|Can Clear")
+	// NoClearの比較用
+	class USkeletalMesh* m_yesClear;
+	
+	// NoClear:	Prevents this object reference from being set to none from the editor.
+	//			Hides clear(and browse) button in the editor.
+	UPROPERTY(NoClear, EditAnywhere, Category = "Clear|Can not Clear")
+	// NoClear指定子でClearボタンとNoneボタンををエディタ上から秘匿します。
+	class USkeletalMesh* m_noClear;
+
+	// Export:	Only useful for object properties (or arrays of objects). 
+	//			Indicates that the object assigned to this property should be exported in its entirety as a subobject block when the object is copied (for copy/paste) or exported to T3D,
+	//			as opposed to just outputting the object reference itself.
+	UPROPERTY(Export)
+	// オブジェクトのみ有効
+	// Exportで指定された変数は、エクスポート時や、コピー/ペースト時にT3D(UnrealEngine独自のテキスト形式)に保存される場合、
+	// そのオブジェクトの<名前やパス、変数値>の代わりに<そのオブジェクトを構成する定義データやインクルードデータ>がテキスト化されます。
+	UmeaninglessObject* m_export;
+
 	// Config:	This property should be loaded/saved to ini file as permanent profile.
-	UPROPERTY(Config = Game)
+	UPROPERTY(Config = "DefaultGame")
 	// Configで指定されたこの変数は指定の.iniファイルへ永続的な数値としてセーブアンドロードされなければなりません。
 	int32 m_config;
 
@@ -155,10 +196,10 @@ public:
 	// サブクラスからは読み込まれません。
 	int32 m_globalConfig;
 
-	// Transient:	Property is transient: shouldn't be saved, zero-filled at load time. Opposite of the SaveGame specifier.
-	UPROPERTY(Transient, BlueprintReadOnly)
-	// Transientで一時的な変数にすることができます。
-	// この変数はセーブできず、ロード時に必ず0が入ります。
-	// SaveGame指定子と同時には扱えません。
-	int32 m_transient;
+	// AssetRegistrySearchable:	This property and it's value will be automatically added to the asset registry for any asset class instances containing this as a member variable.
+	//							It is not legal to use on struct properties or parameters.
+	UPROPERTY(AssetRegistrySearchable, EditAnywhere)
+	// AssetRegistrySearchable指定子をつけるとTagsAndValuesというTMapに格納されます。
+	// この値を編集することによって、この変数自体をタグとしてあつかうことができます。
+	int32 m_assetRegistrySearchable;
 };
